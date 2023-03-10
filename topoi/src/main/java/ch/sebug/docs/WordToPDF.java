3f54f89ab4c9cac436e0adae0546059a2fd95280
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-10 17:19:32 +0000

    Working conversion

--- topoi/src/main/java/ch/sebug/docs/WordToPDF.java
+++ topoi/src/main/java/ch/sebug/docs/WordToPDF.java
@@ -1,9 +1,12 @@
 package ch.sebug.docs;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 
 public class WordToPDF {
@@ -14,7 +17,10 @@ public class WordToPDF {
             var document = new XWPFDocument(inputStream);
             document.createStyles();
             var options = PdfOptions.create();
-            
+            var out = new FileOutputStream(new File(outputPath));
+            var converterInstance = PdfConverter.getInstance();
+            converterInstance.convert(document, out, options);
+            System.out.println("Done");
         } catch (FileNotFoundException fnex) {
             System.err.println(fnex.getMessage());
         } catch (IOException ioe) {
