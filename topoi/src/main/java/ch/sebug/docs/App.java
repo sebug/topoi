9e3127f7de93b6afd1a44efc9afba99e041917b2
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-10 16:18:57 +0000

    POI reference, command line arguments

--- topoi/src/main/java/ch/sebug/docs/App.java
+++ topoi/src/main/java/ch/sebug/docs/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length < 2) {
+            System.err.println("Usage: ch.sebug.docs.app input.docx output.pdf");
+            System.exit(1);
+        }
+        System.out.println( "Converting..." );
     }
 }
